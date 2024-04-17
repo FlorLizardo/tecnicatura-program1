7cbc08fe5a5227d36a62dbdcb986848691b0d017
FlorLizardo <florencializardo7@gmail.com>
2024-04-17 21:18:09 +0200

    En este commit se agregan elementos en directorio java

---
+++ java/leccion1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,16 @@
+//primer hola mundo
+//comentario en linea
+/*comentarios en
+varias lineas*/
+public class HolaMundo {
+    public static void main(String args[]){
+        //main: metodo de java
+        System.out.println("Hola Mundo desde Java");
+        
+    }
+   /*atajo para crear la clase mas rapidamente:
+    psvm + tab 
+    sout + tab -----> atajo para crear el print mas rapido
+    */
+}
+
